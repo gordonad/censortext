c91f938a24f6a150b6e48b31ed381652e9ebfa33
m86014 <gordon.dickens@cigna.com>
2015-07-12 08:27:27 -0400

    initial

---
+++ censortext.js
@@ -0,0 +1,27 @@
+/**
+ * Created by m86014 on 7/12/15.
+ */
+var censoredWords = ["sad", "bad", "mad"];
+var customCensoredWords = [];
+
+function censor(inStr) {
+    for (idx in censoredWords) {
+        inStr = inStr.replace(censoredWords[idx], "****");
+    }
+    for (idx in customCensoredWords) {
+        inStr = inStr.replace(customCensoredWords[idx], "****");
+    }
+    return inStr;
+}
+
+function addCensoredWord(word) {
+    customCensoredWords.push(word);
+}
+
+function getCensoredWords() {
+    return censoredWords.concat(customCensoredWords);
+}
+
+exports.censor = censor;
+exports.addCensoredWord=addCensoredWord;
+exports.getCensoredWords=getCensoredWords;
